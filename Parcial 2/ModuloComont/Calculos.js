206e0f71eca7b0579ed2b4b6fa0f99a99d4bcc16
Alejandro Monsivais 20100239 <alejandromonsivais367@gmail.com>
2023-10-01 17:17:44 -0500

    paquete

--- Parcial 2/ModuloComont/Calculos.js
+++ Parcial 2/ModuloComont/Calculos.js
@@ -3,3 +3,12 @@ let areas= require("./Area.js");
 let a= areas.AreaCuadrado(4);
 
 console.log(a);
+
+let cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
+
