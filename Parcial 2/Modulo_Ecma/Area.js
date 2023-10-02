665fae30b6d150668a408bd60e83d6fcdecebbeb
Alejandro Monsivais 20100239 <alejandromonsivais367@gmail.com>
2023-10-02 09:54:47 -0500

    cambio

---
+++ Parcial 2/Modulo_Ecma/Area.js
@@ -0,0 +1,8 @@
+ export function AreaCuadrado(lado){
+    return lado*lado;
+}
+
+export function AreaTriangulo(base, altura){
+return ((base*altura)/2)
+}
+
---
+++ Parcial 2/Modulo_Ecma/Calculos.js
@@ -0,0 +1,5 @@
+import * as areas from './Area.js';
+
+let a = areas.AreaCuadrado(4);
+
+console.log(a);
